e97ccfc2d7da4ada2abe5daabee816b8850c475f
anonymus-netizien <88516002+anonymus-netizien@users.noreply.github.com>
2026-02-18 16:48:40 +0530

    chore: primitive datatypes

---
+++ java-fundamentals/src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,52 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    static void main(String[] args) {
+        //varaible declaration
+        //byte
+        byte age;
+        age = 20;
+        System.out.println("Age : " + age);
+        byte day, month;
+        day = 16;
+        month = 3;
+        System.out.println("Day : " + day + ", Month : " + month);
+        byte marks;
+        marks = 97;
+        System.out.println("Marks : " + marks);
+        byte discount;
+        discount = 69;
+        System.out.println("Discount : " + discount);
+
+        //short
+        short currentYear;
+        currentYear = 2026;
+        System.out.println("Current Year : " + currentYear);
+        short totalMarks;
+        totalMarks = 970;
+        System.out.println("Total Marks : " + totalMarks);
+
+        //int
+        int numberOfUsers;
+        numberOfUsers = 875793;
+        System.out.println("Number Of Users : " + numberOfUsers);
+        int secondsPerDay;
+        secondsPerDay = 86100;
+        System.out.println("Seconds Per Day : " + secondsPerDay);
+
+        //long
+        long populationOfCountry;
+        populationOfCountry = 87989878787897877L;
+        System.out.println("Population Of Country : " + populationOfCountry);
+        long phoneNumber;
+        phoneNumber = 9878768765L;
+        System.out.println("Phone Number ; " + phoneNumber);
+        long countrysGDP;
+        countrysGDP = 78_47_86_34_76_486L;
+        System.out.println("Countrys GDP : " + countrysGDP);
+        long valuationOfCompany;
+        valuationOfCompany = 87_65_87_59_85_92_485L;
+        System.out.println("Valuation Of The Company : " + valuationOfCompany);
+
+    }
+}
